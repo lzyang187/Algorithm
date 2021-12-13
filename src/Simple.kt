a3a4b119fa543efb660e4917901ab8bb7d27cba6
cyli8 <cyli8@iflytek.com>
2021-12-13 20:34:55 +0800

    整数反转

--- src/Simple.kt
+++ src/Simple.kt
@@ -1,6 +1,5 @@
 fun main() {
-    val a = intArrayOf(1, 2, 3)
-    twoSum(a, 5)
+    println(reverse(-120))
 }
 
 
@@ -25,4 +24,35 @@ fun twoSum(nums: IntArray?, target: Int): IntArray {
         }
     }
     return result
+}
+
+/**
+ * 整数反转：
+ * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+ * 如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+ * 假设环境不允许存储 64 位整数（有符号或无符号）。
+ */
+
+fun reverse(x: Int): Int {
+    val str = x.toString()
+    val split = str.split("")
+    if (split.isNullOrEmpty()) {
+        return 0
+    }
+    val filter = split.filter {
+        it != ""
+    }
+    val sb = StringBuilder()
+    for (i in filter.size - 1 downTo 1) {
+        if (filter[i] == "0" && sb.isEmpty()) {
+            continue
+        }
+        sb.append(filter[i])
+    }
+    val result: String = if (filter[0] == "-") {
+        ("-$sb")
+    } else {
+        sb.append(filter[0]).toString()
+    }
+    return result.toIntOrNull() ?: 0
 }
