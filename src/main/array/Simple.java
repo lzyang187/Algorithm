9b9b688c2aa7bde1fbd185209be6e54581eef7e6
zhaoyang.li5 <zhaoyang.li5@transsion.com>
2024-09-09 20:24:02 +0800

    1、提交gitignore 2、提莫攻击问题，不能用Set，会超时

--- src/main/array/Simple.java
+++ src/main/array/Simple.java
@@ -790,7 +790,11 @@ public class Simple {
     }
 
     /**
-     * 提莫攻击
+     * 提莫攻击：当提莫攻击艾希，艾希的中毒状态正好持续 duration 秒。
+     * 正式地讲，提莫在 t 发起攻击意味着艾希在时间区间 [t, t + duration - 1]（含 t 和 t + duration - 1）处于中毒状态。
+     * 如果提莫在中毒影响结束 前 再次攻击，中毒状态计时器将会 重置 ，在新的攻击之后，中毒影响将会在 duration 秒后结束。
+     * 给你一个 “非递减” 的整数数组 timeSeries ，其中 timeSeries[i] 表示提莫在 timeSeries[i] 秒时对艾希发起攻击，以及一个表示中毒持续时间的整数 duration 。
+     * 返回艾希处于中毒状态的 总 秒数。
      */
     public int findPoisonedDuration(int[] timeSeries, int duration) {
         if (timeSeries == null || timeSeries.length <= 0 || duration <= 0) {
@@ -800,16 +804,16 @@ public class Simple {
             return duration;
         }
         int sum = 0;
-        for (int i = 0; i < timeSeries.length - 1; i++) {
+        for (int i = 0; i < timeSeries.length; i++) {
             int time = timeSeries[i] + duration - 1;
-            if (time < timeSeries[i + 1]) {
+            // 注意判断越界情况
+            if ((i + 1) == timeSeries.length || time < timeSeries[i + 1]) {
                 sum += duration;
             } else {
                 sum += timeSeries[i + 1] - timeSeries[i];
             }
         }
-        // 加上最后一次攻击的时间
-        return sum + duration;
+        return sum;
     }
 
 }
